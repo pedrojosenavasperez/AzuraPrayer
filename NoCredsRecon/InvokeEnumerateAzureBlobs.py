5ee0237355f34191f1f435dad9e44ac0e1fd32e3
Pedro J. Navas <pedrojosenavasperez@gmail.com>
2022-11-17 00:17:07 +0100

    Blobs 1.0

---
+++ NoCredsRecon/InvokeEnumerateAzureBlobs.py
@@ -0,0 +1,58 @@
+from dns import resolver
+from tabulate import tabulate
+
+# Based on https://github.com/NetSPI/MicroBurst/blob/master/Misc/Invoke-EnumerateAzureSubDomains.ps1
+# Enumerate public Azure Blobs and Containers.
+# The function will check for valid .blob.core.windows.net host names via DNS
+
+
+final_table=[]
+
+def BaseQuery(base, verbose=False):
+	if(verbose):
+		print("Fetching " + base +".blob.core.windows.net" )
+	try:
+		result = resolver.resolve(rdtype="A_AAAA" ,  qname=base+".blob.core.windows.net·" , raise_on_no_answer=False )
+		if(result):
+			final_table.append("Found Storage Account "+ base +".blob.core.windows.net" )
+	except:
+			pass
+
+
+def EnumerateAzureBlobs(base="" , permutations="permutations.txt" , verbose=False , folders="permutations.txt" , OutputFile="" , BingAPIKey=""):
+	domain = '.blob.core.windows.net'
+	if(base==""):
+		print("Not base name especified.")
+	else:
+
+		BaseQuery(base)
+
+		try:
+			with open(permutations) as permutations_file:
+				for line in permutations_file:
+					line_strip = line.rstrip()
+					BaseQuery(line_strip+base)
+					BaseQuery(base+line_strip)
+		except:
+			print("Invalid permutation file.")
+		
+		print(tabulate(final_table, headers=['Findings'], tablefmt='fancy_grid'))
+
+		if(verbose):
+				print("DNS bruteforce finished , starting container enumeration")
+
+		if(BingAPIKey != ""):
+			BingQuery = "site:blob.core.windows.net "+ Base
+			# To DO implement this
+
+		for domain in final_table:
+			try:
+				with open(permutations) as permutations_file:
+					for line in permutations_file:
+						line_strip = line.rstrip()
+						BaseQuery(line_strip+base)
+						BaseQuery(base+line_strip)
+			except:
+				print("Invalid permutation file.")
+				
+
